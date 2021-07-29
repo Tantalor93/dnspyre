7b8ae30d5164a4369fe80f69c2421c3eeff66b3d
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2021-07-29 08:42:31 +0200

    barplot

--- cmd/dnstrace/plot.go
+++ cmd/dnstrace/plot.go
@@ -2,14 +2,16 @@ package dnstrace
 
 import (
 	"fmt"
+	"image/color"
 	"os"
 
+	"github.com/miekg/dns"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 )
 
-func plotHistogram(file string, times []datapoint) {
+func plotHistogramLatency(file string, times []datapoint) {
 	var values plotter.Values
 	for _, v := range times {
 		values = append(values, v.duration)
@@ -30,7 +32,7 @@ func plotHistogram(file string, times []datapoint) {
 	}
 }
 
-func plotBoxPlot(file, server string, times []datapoint) {
+func plotBoxPlotLatency(file, server string, times []datapoint) {
 	var values plotter.Values
 	for _, v := range times {
 		values = append(values, v.duration)
@@ -51,7 +53,7 @@ func plotBoxPlot(file, server string, times []datapoint) {
 	}
 }
 
-func plotLine(file string, times []datapoint) {
+func plotLineLatency(file string, times []datapoint) {
 	var values plotter.XYs
 	for i, v := range times {
 		values = append(values, plotter.XY{X: float64(i), Y: v.duration})
@@ -66,6 +68,30 @@ func plotLine(file string, times []datapoint) {
 		panic(err)
 	}
 	p.Add(l)
+
+	if err := p.Save(6*vg.Inch, 6*vg.Inch, file); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to save plot.", err)
+	}
+}
+
+func plotResponses(file string, rcodes map[int]int64) {
+	var values plotter.Values
+	var names []string
+	for k, v := range rcodes {
+		values = append(values, float64(v))
+		names = append(names, dns.RcodeToString[k])
+	}
+	p := plot.New()
+	p.Title.Text = "Responses distribution"
+
+	barchart, err := plotter.NewBarChart(values, vg.Length(60))
+	if err != nil {
+		panic(err)
+	}
+	p.Add(barchart)
+	p.NominalX(names...)
+	p.Y.Label.Text = "count"
+	barchart.Color = color.Gray{Y: 128}
 
 	if err := p.Save(6*vg.Inch, 6*vg.Inch, file); err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to save plot.", err)
