d08e82813699cf5a69d6d78b1975ed17c0560973
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2022-02-16 16:11:27 +0100

    improve release script

--- release.sh
+++ release.sh
@@ -4,6 +4,17 @@ if [ "$#" -ne 1 ]; then
   echo "tag argument missing" >&2
   exit 1
 fi
+
+if ! command -v ghr &> /dev/null
+  then echo "ghr tool is not installed" >&2
+  exit 1
+fi
+
 echo "releasing tag $1"
-make VERSION="$1" build
-ghr "$1" bin/
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "GITHUB_TOKEN variable is not set" >&2
+  exit 1
+fi
+
+make VERSION="$1" build && ghr "$1" bin/
