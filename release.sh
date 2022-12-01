802f57b409bcd143d7ce94d528d477da4b91e49d
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2022-12-01 13:41:26 +0100

    clean release script

--- release.sh
+++ release.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-  echo "release version and message is required, 'sh release.sh <version> <message>'" >&2
+if [ "$#" -ne 1 ]; then
+  echo "specify only release version, 'sh release.sh <version>'" >&2
+  exit 1
+fi
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "GITHUB_TOKEN env variable is missing" >&2
+    exit 1
+fi
+
+if ! command -v goreleaser &> /dev/null
+  then echo "goreleaser tool is not installed" >&2
   exit 1
 fi
 
 echo "releasing tag $1"
-git tag -a $1
-git push origin $1
+git tag -a "$1"
+git push origin "$1"
 goreleaser release --rm-dist
