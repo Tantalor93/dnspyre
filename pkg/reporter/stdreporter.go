e2e371ad12e8e8c6ef243bc51ae84b3122f7fd41
Ondřej Benkovský <ondrej.benkovsky@sentinelone.com>
2025-06-22 20:26:03 +0200

    fix json report histogram deduplication

--- pkg/reporter/stdreporter.go
+++ pkg/reporter/stdreporter.go
@@ -184,19 +184,3 @@ func makeBar(c int64, max int64) string {
 	t := int((43 * float64(c) / float64(max)) + 0.5)
 	return strings.Repeat(printutils.HighlightSprint("▄"), t)
 }
-
-func roundDuration(dur time.Duration) time.Duration {
-	if dur > time.Minute {
-		return dur.Round(10 * time.Second)
-	}
-	if dur > time.Second {
-		return dur.Round(10 * time.Millisecond)
-	}
-	if dur > time.Millisecond {
-		return dur.Round(10 * time.Microsecond)
-	}
-	if dur > time.Microsecond {
-		return dur.Round(10 * time.Nanosecond)
-	}
-	return dur
-}
